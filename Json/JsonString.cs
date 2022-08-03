f5bf2a79f2bb584757f23be730cdc4476c58e37e
Mitsutoshi Nakano <ItSANgo@gmail.com>
2022-08-04 06:36:39 +0900

    プロジェクト ファイルを追加します。

---
+++ Json/JsonString.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using System;
+using System.Net;
+
+namespace JsonSerializer
+{
+    class JsonSerializer
+    {
+        public static string Serialize<TValue>(TValue value, JsonSerializerOptions options = default)
+        {
+            return System.Text.Json.JsonSerializer.Serialize<TValue>(value, options);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine(Serialize(new WebProxy("http://127.0.0.1:8080/")));
+        }
+    }
+}
